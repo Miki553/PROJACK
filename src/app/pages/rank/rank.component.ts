d51cb0b9543e3faf0cc42a4fc5b2e92234ce1e3a
Miki <161672000+Miki553@users.noreply.github.com>
2024-03-18 18:48:18 +0700

    Add files via upload

--- src/app/pages/rank/rank.component.ts
+++ src/app/pages/rank/rank.component.ts
@@ -52,7 +52,7 @@ export class RankComponent {
       this.Top10();
     });
 
-    this.reface();
+    // this.reface();
 
   }
 
@@ -71,20 +71,13 @@ reface(){
 }
 
   
-
-changData(i : number){
-    this.imgshow = this.Top10User[i].image;
-    this.nameshow = this.Top10User[i].title;
-    this.http.get(`${this.apiurl}/statistics/chat/`+this.Top10User[i].image_id).subscribe((data : any) => {this.ngAfterViewInit(data);});
-}
-
-
-
 Top10(){
   this.rank = [];
   this.http.get(`${this.apiurl}/image/top10`).pipe(
     switchMap((data: any) => {
       this.Top10User = data;
+      console.log(this.Top10User);
+      
       const requests = this.Top10User.map((img : any) => {
         return this.http.get(`${this.apiurl}/statistics/chat/` + img.image_id);
       });
@@ -93,85 +86,138 @@ Top10(){
   ).subscribe((responses: any) => {
     responses.forEach((data2: any) => {
       if(data2 && data2.length >= 2) { 
-        const sum = data2[1].rank - data2[0].rank;
+        // const sum = data2[1].rank - data2[0].rank;
+        const sum = data2[1].rank;
         this.rank.push(sum);
       } else {
         this.rank.push(0);
       }
     });
-
-    // console.log(this.rank);
-
-    this.imgshow = this.Top10User[0].image;
-    this.nameshow = this.Top10User[0].title;
-    this.http.get(`${this.apiurl}/statistics/chat/`+this.Top10User[0].image_id).subscribe((data : any) => {this.ngAfterViewInit(data);});
+    this.ngAfterViewInit(this.Top10User);
   });
 }
 
-ngAfterViewInit(scorea:any): void {
-  let num=[];
-  if (scorea && scorea.length > 0) {
-    for(let s of scorea){
-      num.push(s.score);
-    }
-  } 
+// ngAfterViewInit(scorea:any): void {
+//   let num=[];
+//   if (scorea && scorea.length > 0) {
+//     for(let s of scorea){
+//       num.push(s.score);
+//     }
+//   } 
 
 
-  const data = [
-    // { day: this.getday(0), count:  num[0]},
-    // { day: this.getday(1), count: num[1] },
-    // { day: this.getday(2), count: num[2] },
-    // { day: this.getday(3), count: num[3] },
-    // { day: this.getday(4), count: num[4] },
-    // { day: this.getday(5), count: num[5] },
-    // { day: this.getday(6), count: num[6] },
+//   const data = [
+//     // { day: this.getday(0), count:  num[0]},
+//     // { day: this.getday(1), count: num[1] },
+//     // { day: this.getday(2), count: num[2] },
+//     // { day: this.getday(3), count: num[3] },
+//     // { day: this.getday(4), count: num[4] },
+//     // { day: this.getday(5), count: num[5] },
+//     // { day: this.getday(6), count: num[6] },
 
-    { day: this.getday(6), count: num[6] },
-    { day: this.getday(5), count: num[5] },
-    { day: this.getday(4), count: num[4] },
-    { day: this.getday(3), count: num[3] },
-    { day: this.getday(2), count: num[2] },
-    { day: this.getday(1), count: num[1] },
-    { day: this.getday(0), count:  num[0]},
-  ];
+//     { day: this.getday(6), count: num[6] },
+//     { day: this.getday(5), count: num[5] },
+//     { day: this.getday(4), count: num[4] },
+//     { day: this.getday(3), count: num[3] },
+//     { day: this.getday(2), count: num[2] },
+//     { day: this.getday(1), count: num[1] },
+//     { day: this.getday(0), count:  num[0]},
+//   ];
 
   
 
-  const canvas = document.getElementById('acquisitions') as HTMLCanvasElement;
-  if (canvas) {
-    const existingChart = Chart.getChart(canvas);
-    if (existingChart) {
-      existingChart.destroy();
-    }
-    const chart = new Chart(
-      canvas,
-      {
-        type: 'line',
-        options: {
-          animation: false,
-          plugins: {
-            legend: {
-              display: false
-            },
-            tooltip: {
-              enabled: false
-            }
-          }
-        },
-        data: {
-          labels: data.map(row => row.day),
-          datasets: [
-            {
-              label: 'Acquisitions by year',
-              data: data.map(row => row.count)
-            }
-          ]
-        }
-      }
-    );
+//   const canvas = document.getElementById('acquisitions') as HTMLCanvasElement;
+//   if (canvas) {
+//     const existingChart = Chart.getChart(canvas);
+//     if (existingChart) {
+//       existingChart.destroy();
+//     }
+//     const chart = new Chart(
+//       canvas,
+//       {
+//         type: 'line',
+//         options: {
+//           animation: false,
+//           plugins: {
+//             legend: {
+//               display: false
+//             },
+//             tooltip: {
+//               enabled: false
+//             }
+//           }
+//         },
+//         data: {
+//           labels: data.map(row => row.day),
+//           datasets: [
+//             {
+//               label: 'Acquisitions by year',
+//               data: data.map(row => row.count)
+//             }
+//           ]
+//         }
+//       }
+//     );
 
+//   }
+// }
+
+ngAfterViewInit(scorea: any[]): void {
+  if (scorea && scorea.length > 0) {
+      scorea.forEach((s, index) => {
+          const canvasId = `acquisitions${index}`;
+          const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+          if (canvas) {
+              const existingChart = Chart.getChart(canvas);
+              if (existingChart) {
+                  existingChart.destroy();
+              }
+             
+              this.http.get(`${this.apiurl}/statistics/chat/`+s.image_id).subscribe((data2 : any) => {
+                let num =[];
+                for(let stt of data2){
+                  num.push(stt.score);
+                }
+                
+                // console.log(num);
+                
+                
+              const data = [
+                { day: this.getday(6), count: num[6] },
+                { day: this.getday(5), count: num[5] },
+                { day: this.getday(4), count: num[4] },
+                { day: this.getday(3), count: num[3] },
+                { day: this.getday(2), count: num[2] },
+                { day: this.getday(1), count: num[1] },
+                { day: this.getday(0), count:  num[0]},
+              ];
+
+                const chart = new Chart(
+                  canvas,
+                  {
+                      type: 'line',
+                      options: {
+                          animation: false,
+                          plugins: {
+                              legend: { display: false },
+                              tooltip: { enabled: false }
+                          }
+                      },
+                      data: {
+                          labels: data.map(row => row.day),
+                          datasets: [{ label: 'Acquisitions by year', data: data.map(row => row.count) }]
+                      }
+                  }
+              );
+              });
+    
+
+
+          }
+      });
   }
 }
+
 
 getday(num : number){
   const currentDate = new Date();
