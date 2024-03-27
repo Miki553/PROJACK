bd77e3bf2a8435c0e29e6941b124b861650a3f7f
Miki <161672000+Miki553@users.noreply.github.com>
2024-03-28 01:56:09 +0700

    Update appdata.service.ts

--- src/app/service/appdata.service.ts
+++ src/app/service/appdata.service.ts
@@ -6,25 +6,15 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class AppdataService {
   private User = null;
-//   private User = {        
-//             "user_id": 13,
-//             "username": "Snack",
-//             "email": "Snack4234@gmail.com",
-//             "password": "Snack4234",
-//             "avatar": "https://firebasestorage.googleapis.com/v0/b/api-test-94f3b.appspot.com/o/images%2Fdda.jpg?alt=media&token=3dbdd160-bda9-4491-8a2f-49d952563db9",
-//             "avatar_name": "dda.jpg",
-//             "type": "Users"
-// };
+
   // private apiurl = "https://apiprojac.onrender.com";
   // private apiurl = "http://localhost:3000";
       private apiurl = "https://vercel-api-gray.vercel.app";
 
 
-  // private headerDataSubject = new BehaviorSubject<string>('https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png');
+ 
   private headerDataSubject = new BehaviorSubject<string>('https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png');
-  constructor() { 
-    // this.updateHeaderData(this.User.avatar);
-  }
+  constructor() {}
 
 
 
